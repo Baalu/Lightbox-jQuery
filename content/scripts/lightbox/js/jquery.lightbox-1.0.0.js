e75b586c1337c7b7f9a5e62d59502b270ac29d73
Balakumar <kumar.bala17@gmail.com>
2014-10-16 22:14:14 +0530

    changes

--- content/scripts/lightbox/js/jquery.lightbox-1.0.0.js
+++ content/scripts/lightbox/js/jquery.lightbox-1.0.0.js
@@ -2,9 +2,9 @@
  * jQuery lightBox plugin
  * This jQuery plugin was inspired and based on Lightbox 2 by Lokesh Dhakar (http://www.huddletogether.com/projects/lightbox2/)
  * and adapted to me for use like a plugin from jQuery.
- * @name jquery-lightbox-0.5.js
+ * @name jquery-lightbox-js
  * @author Leandro Vieira Pinho - http://leandrovieira.com
- * @version 0.5
+ * @version 1.0.0
  * @date April 11, 2008
  * @category jQuery plugin
  * @copyright (c) 2008 Leandro Vieira Pinho (leandrovieira.com)
